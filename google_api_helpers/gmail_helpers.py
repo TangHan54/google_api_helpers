2ca8a188262b76f2a8b0c92c4a242285aa530a96
Han Tang <han.tang@shopee.com>
2021-03-31 15:41:44 +0800

    added encoders for email

--- google_api_helpers/gmail_helpers.py
+++ google_api_helpers/gmail_helpers.py
@@ -2,6 +2,7 @@ import base64
 import smtplib
 import mimetypes
 
+from email import encoders
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
@@ -62,6 +63,7 @@ class Mail:
                 msg = MIMEBase(main_type, sub_type)
                 msg.set_payload(fp.read())
                 fp.close()
+            encoders.encode_base64(msg)
             filename = os.path.basename(file)
             msg.add_header("Content-Disposition", "attachment", filename=filename)
             message.attach(msg)
